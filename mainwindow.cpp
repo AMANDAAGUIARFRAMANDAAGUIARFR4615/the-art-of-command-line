b8fe0dbaa69a3f135e5978d7d2b43fe8ff1f5b66
test <test@gmail.com>
2025-09-15 20:50:35 +0800

    布局

---
+++ mainwindow.cpp
@@ -0,0 +1,65 @@
+#include "mainwindow.h"
+#include <QTabWidget>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QGridLayout>
+#include <QLabel>
+#include <QFrame>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    QWidget *central = new QWidget(this);
+    QVBoxLayout *mainLayout = new QVBoxLayout(central);
+
+    // 上方大区域：Tab 标签页
+    QTabWidget *tabWidget = new QTabWidget(this);
+    QWidget *tab1 = new QWidget();
+    QWidget *tab2 = new QWidget();
+    QWidget *tab3 = new QWidget();
+
+    tab1->setLayout(new QVBoxLayout());
+    tab1->layout()->addWidget(new QLabel("这是标签页 1 的内容"));
+    tab2->setLayout(new QVBoxLayout());
+    tab2->layout()->addWidget(new QLabel("这是标签页 2 的内容"));
+    tab3->setLayout(new QVBoxLayout());
+    tab3->layout()->addWidget(new QLabel("这是标签页 3 的内容"));
+
+    tabWidget->addTab(tab1, "Tab 1");
+    tabWidget->addTab(tab2, "Tab 2");
+    tabWidget->addTab(tab3, "Tab 3");
+
+    // 下方 2 行 4 列的小布局区域
+    QWidget *bottomWidget = new QWidget(this);
+    QGridLayout *gridLayout = new QGridLayout(bottomWidget);
+
+    for (int row = 0; row < 2; ++row)
+    {
+        for (int col = 0; col < 4; ++col)
+        {
+            QFrame *frame = new QFrame(bottomWidget);
+            frame->setFrameShape(QFrame::Box);
+            frame->setLineWidth(2);
+
+            QLabel *label = new QLabel(QString("区域 %1,%2").arg(row + 1).arg(col + 1), frame);
+            label->setAlignment(Qt::AlignCenter);
+
+            QVBoxLayout *frameLayout = new QVBoxLayout(frame);
+            frameLayout->addWidget(label);
+
+            gridLayout->addWidget(frame, row, col);
+        }
+    }
+
+    // 上下布局，比例 2:1
+    mainLayout->addWidget(tabWidget, 2);
+    mainLayout->addWidget(bottomWidget, 1);
+
+    setCentralWidget(central);
+    setWindowTitle("Qt 多布局示例");
+    resize(800, 600);
+}
+
+MainWindow::~MainWindow()
+{
+}
