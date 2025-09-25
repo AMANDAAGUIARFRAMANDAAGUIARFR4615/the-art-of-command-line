fc3f56096e5f453e4d9d9a6cf9dd1157ff115d45
test <test@gmail.com>
2025-09-25 15:56:50 +0800

    __PROJECT_FILE__

--- Logger.h
+++ Logger.h
@@ -2,5 +2,7 @@
 
 #include <QDateTime>
 
-#define qDebugEx() qDebug().noquote() << "[" << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss") << "]" << __FILE__ << __LINE__ << __FUNCTION__
-#define qCriticalEx() qCritical().noquote() << "[" << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss") << "]" << __FILE__ << __LINE__ << __FUNCTION__
+#define __PROJECT_FILE__ (__FILE__ + sizeof(PROJECT_SOURCE_DIR))
+
+#define qDebugEx() qDebug().noquote() << "[" << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss") << "]" << __PROJECT_FILE__ << __LINE__ << __FUNCTION__
+#define qCriticalEx() qCritical().noquote() << "[" << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss") << "]" << __PROJECT_FILE__ << __LINE__ << __FUNCTION__
