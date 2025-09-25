e7d38dad4be326a306e88f685bea3bc2599b6c5b
test <test@gmail.com>
2025-09-25 16:45:01 +0800

    x

--- LogWindow.h
+++ LogWindow.h
@@ -1,21 +1,38 @@
 #ifndef LOGWINDOW_H
 #define LOGWINDOW_H
 
-#include <QWidget>
 #include <QTextBrowser>
+#include <QMetaObject>
+#include <QMessageLogContext>
 
-class LogWindow : public QWidget
+class LogWindow : public QTextBrowser
 {
     Q_OBJECT
-
 public:
-    explicit LogWindow(QWidget *parent = nullptr);
+    explicit LogWindow(QWidget *parent = nullptr) : QTextBrowser(parent)
+    {
+        logWindow = this;
 
-    void append(const QString &text);
-    void toggleVisibility();
+        qInstallMessageHandler([](QtMsgType type, const QMessageLogContext &context, const QString &message) {
+            QMetaObject::invokeMethod(logWindow, [type, message]() {
+                if (type == QtCriticalMsg || type == QtFatalMsg || type == QtWarningMsg) {
+                    // 红色显示错误、警告、致命信息
+                    logWindow->append(QString("<span style='color:red;'>%1</span>").arg(message.toHtmlEscaped()));
+                } else {
+                    // 普通信息默认黑色
+                    logWindow->append(message.toHtmlEscaped());
+                }
+            });
+        });
+    }
+
+    void toggleVisibility()
+    {
+        setVisible(!isVisible());
+    }
 
 private:
-    QTextBrowser *logText;
+    inline static LogWindow* logWindow = nullptr;
 };
 
 #endif // LOGWINDOW_H
