5d67a8c04ffc0cce35ed6f632b0d2664788890a0
test <test@gmail.com>
2025-09-26 21:39:44 +0800

    EventHub.cpp

---
+++ EventHub.cpp
@@ -0,0 +1,45 @@
+#include "EventHub.h"
+
+QMap<QString, QList<std::pair<std::function<void(QJsonObject, QTcpSocket*)>, int>>> EventHub::listeners;
+
+void EventHub::StartListening(const QString& eventName, std::function<void(QJsonObject, QTcpSocket*)> listener, int priority)
+{
+    listeners[eventName].emplace_back(listener, priority);
+
+    auto& actions = listeners[eventName];
+    std::sort(actions.begin(), actions.end(), [](const auto& a, const auto& b)
+    {
+        return a.second > b.second;
+    });
+}
+
+void EventHub::StopListening(const QString& eventName, std::function<void(QJsonObject, QTcpSocket*)> listener)
+{
+    if (listeners.contains(eventName))
+    {
+        auto& actions = listeners[eventName];
+
+        if (listener == nullptr)
+        {
+            actions.clear();
+        }
+        else
+        {
+            actions.erase(
+                std::remove_if(actions.begin(), actions.end(), 
+                               [&listener](const auto& pair) { return &pair.first == &listener; }),
+                actions.end());
+        }
+    }
+}
+
+void EventHub::TriggerEvent(const QString& eventName, const QJsonObject& data, QTcpSocket* socket)
+{
+    if (listeners.contains(eventName))
+    {
+        for (const auto& action : listeners[eventName])
+        {
+            action.first(data, socket);
+        }
+    }
+}
--- EventHub.h
+++ EventHub.h
@@ -15,52 +15,12 @@
 class EventHub
 {
 public:
-    static void StartListening(const QString& eventName, std::function<void(QJsonObject, QTcpSocket*)> listener, int priority = 0)
-    {
-        listeners[eventName].emplace_back(listener, priority);
-
-        auto& actions = listeners[eventName];
-        std::sort(actions.begin(), actions.end(), [](const auto& a, const auto& b)
-        {
-            return a.second > b.second;
-        });
-    }
-
-    static void StopListening(const QString& eventName, std::function<void(QJsonObject, QTcpSocket*)> listener = nullptr)
-    {
-        if (listeners.contains(eventName))
-        {
-            auto& actions = listeners[eventName];
-
-            if (listener == nullptr)
-            {
-                actions.clear();
-            }
-            else
-            {
-                actions.erase(
-                    std::remove_if(actions.begin(), actions.end(), 
-                                   [&listener](const auto& pair) { return &pair.first == &listener; }),
-                    actions.end());
-            }
-        }
-    }
-
-    static void TriggerEvent(const QString& eventName, const QJsonObject& data = QJsonObject(), QTcpSocket* socket = nullptr)
-    {
-        if (listeners.contains(eventName))
-        {
-            for (const auto& action : listeners[eventName])
-            {
-                action.first(data, socket);
-            }
-        }
-    }
+    static void StartListening(const QString& eventName, std::function<void(QJsonObject, QTcpSocket*)> listener, int priority = 0);
+    static void StopListening(const QString& eventName, std::function<void(QJsonObject, QTcpSocket*)> listener = nullptr);
+    static void TriggerEvent(const QString& eventName, const QJsonObject& data = QJsonObject(), QTcpSocket* socket = nullptr);
 
 private:
     static QMap<QString, QList<std::pair<std::function<void(QJsonObject, QTcpSocket*)>, int>>> listeners;
 };
-
-QMap<QString, QList<std::pair<std::function<void(QJsonObject, QTcpSocket*)>, int>>> EventHub::listeners;
 
 #endif // EVENTHUB_H
