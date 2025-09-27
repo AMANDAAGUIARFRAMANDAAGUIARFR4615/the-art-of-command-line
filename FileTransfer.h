20ca538fcd9fd930295d4a992a729c66ac8c2f3e
test <test@gmail.com>
2025-09-27 19:39:57 +0800

    发送完成断开连接

--- FileTransfer.h
+++ FileTransfer.h
@@ -5,40 +5,47 @@
 #include <QTcpServer>
 #include <QFile>
 #include <QDataStream>
-#include <QDebug>
-#include <QTextStream>
 
-class FileTransfer : public QTcpServer {
+class FileTransfer : public QTcpServer
+{
     Q_OBJECT
 public:
-    FileTransfer(int type, const QString& path, const QString& md5, qint64 size): path(path), md5(md5), size(size) {
+    FileTransfer(int type, const QString &path, const QString &md5, quint64 size) : path(path), md5(md5), size(size)
+    {
         connect(this, &QTcpServer::newConnection, this, &FileTransfer::onNewConnection);
 
-        if (!listen(QHostAddress::Any, 0)) {
+        if (!listen(QHostAddress::Any, 0))
+        {
             qWarning() << "Server failed to start";
-        } else {
+        }
+        else
+        {
             qDebugEx() << "Server started, waiting for connections...";
         }
     }
 
 protected:
-    void onNewConnection() {
+    void onNewConnection()
+    {
         auto socket = nextPendingConnection();
         connect(socket, &QTcpSocket::readyRead, this, &FileTransfer::onReadyRead);
         connect(socket, &QTcpSocket::disconnected, socket, &QTcpSocket::deleteLater);
 
         QFile file(path);
-        
-        if (!file.open(QIODevice::ReadOnly)) {
+
+        if (!file.open(QIODevice::ReadOnly))
+        {
             qCriticalEx() << "Failed to open file for sending.";
             return;
         }
 
-        qint64 bytesSent = 0;
-        while (!file.atEnd()) {
+        quint64 bytesSent = 0;
+        while (!file.atEnd())
+        {
             auto buffer = file.read(4096);
             qint64 written = socket->write(buffer);
-            if (written == -1) {
+            if (written == -1)
+            {
                 qCriticalEx() << "文件发送失败";
                 break;
             }
@@ -50,19 +57,31 @@ protected:
         }
 
         file.close();
-        socket->close();
-        close();
         qInfoEx() << "文件发送成功";
     }
 
-    void onReadyRead() {
+    void onReadyRead()
+    {
         QTcpSocket *socket = qobject_cast<QTcpSocket *>(sender());
         auto data = socket->readAll();
-        qDebugEx() << "onReadyRead" << data.length();
+        
+        buffer.append(data);
+
+        while (buffer.size() >= 8)
+        {
+            auto bytesSent = *reinterpret_cast<quint64 *>(buffer.data());
+            buffer.remove(0, 8);
+
+            if (size == bytesSent) {
+                close();
+                qDebugEx() << "发送完成断开连接";
+            }
+        }
     }
 
 private:
     QString path;
     QString md5;
-    qint64 size;
+    quint64 size;
+    QByteArray buffer;
 };
